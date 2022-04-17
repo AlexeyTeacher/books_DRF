897613745ee1adc3db623a9d460ffccf81a9154f
AlexeyTeacher <ak45rus@gmail.com>
2022-04-18 01:40:41 +0300

    add permission

--- store/tests/test_api.py
+++ store/tests/test_api.py
@@ -12,10 +12,14 @@ from store.serializers import BookSerializer
 class BooksApiTestCase(APITestCase):
     def setUp(self):
         self.user = User.objects.create(username='test_username')
-        self.book_1 = Book.objects.create(name='Test book 1', price=100, author_name='Author 1')
-        self.book_2 = Book.objects.create(name='Test book 2 ', price=1400, author_name='Author 2')
-        self.book_3 = Book.objects.create(name='Test book 3', price=666, author_name='Author 1')
-        self.book_4 = Book.objects.create(name='Author 1 Test book 3', price=100, author_name='Author 3')
+        self.book_1 = Book.objects.create(name='Test book 1', price=100,
+                                          author_name='Author 1', owner=self.user)
+        self.book_2 = Book.objects.create(name='Test book 2 ', price=1400,
+                                          author_name='Author 2')
+        self.book_3 = Book.objects.create(name='Test book 3', price=666,
+                                          author_name='Author 1')
+        self.book_4 = Book.objects.create(name='Author 1 Test book 3', price=100,
+                                          author_name='Author 3')
 
     def test_get(self):
         url = reverse('book-list')
@@ -58,6 +62,7 @@ class BooksApiTestCase(APITestCase):
         response = self.client.post(url, data=json_data, content_type='application/json')
         self.assertEqual(status.HTTP_201_CREATED, response.status_code)
         self.assertEqual(5, Book.objects.all().count())
+        self.assertEqual(self.user, Book.objects.last().owner)
 
     def test_update(self):
         url = reverse('book-detail', args=(self.book_1.id,))
@@ -80,3 +85,33 @@ class BooksApiTestCase(APITestCase):
         response = self.client.delete(url)
         self.assertEqual(status.HTTP_204_NO_CONTENT, response.status_code)
         self.assertEqual(3, Book.objects.all().count())
+
+    def test_update_not_owner(self):
+        self.user2 = User.objects.create(username='test_username2')
+        url = reverse('book-detail', args=(self.book_1.id,))
+        data = {
+            "name": self.book_1.name,
+            "price": 1014,
+            "author_name": self.book_1.author_name
+        }
+        json_data = json.dumps(data)
+        self.client.force_login(self.user2)
+        response = self.client.put(url, data=json_data, content_type='application/json')
+        self.assertEqual(status.HTTP_403_FORBIDDEN, response.status_code)
+        self.book_1.refresh_from_db()
+        self.assertEqual(100, self.book_1.price)
+
+    def test_update_not_owner_but_staff(self):
+        self.user2 = User.objects.create(username='test_username2', is_staff=True)
+        url = reverse('book-detail', args=(self.book_1.id,))
+        data = {
+            "name": self.book_1.name,
+            "price": 1014,
+            "author_name": self.book_1.author_name
+        }
+        json_data = json.dumps(data)
+        self.client.force_login(self.user2)
+        response = self.client.put(url, data=json_data, content_type='application/json')
+        self.assertEqual(status.HTTP_200_OK, response.status_code)
+        self.book_1.refresh_from_db()
+        self.assertEqual(1014, self.book_1.price)
--- store/views.py
+++ store/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import SearchFilter, OrderingFilter
-from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
 from rest_framework.viewsets import ModelViewSet
 from .models import Book
+from .permissions import IsOwnerOrStaffOrReadOnly
 from .serializers import BookSerializer
 
 
@@ -11,10 +11,15 @@ class BookViewSet(ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
-    permission_classes = [IsAuthenticatedOrReadOnly]
+    permission_classes = [IsOwnerOrStaffOrReadOnly]
     filter_fields = ['price']
     search_fields = ['name', 'author_name']
     ordering_fields = ['price', 'author_name']
+
+    def perform_create(self, serializer):
+        serializer.validated_data['owner'] = self.request.user
+        serializer.save()
+
 
 
 def auth(request):
